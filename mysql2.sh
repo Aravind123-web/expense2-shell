01a5c05392ee6dd4688ad20cd8712c2e1278e8f3
Thangella Aravind <aravindthangella1997@gmail.com>
2025-12-17 22:57:24 +0530

    mysql server install

---
+++ mysql2.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=&(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.LOG
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+
+    if [$1 -ne 0]
+    then 
+      echo -e "$2...$R FAILURE $N"
+      exit 1
+    else
+      echo -e "$2...$G SUCCESS $N"
+    fi
+}
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script as root user"
+    exit 2
+else
+    echo "You are root user, you can proceed"
+fi
+
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing Mysql server"
+
+dnf enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling Mysql server"
+
+dnf start mysqld &>>$LOGFILE
+VALIDATE $? "Starting Mysql server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+VALIDATE $? "Setting Mysql root password"
